52213de37d475fa101fd4db12f05136a17f2f5b8
Lukienko <sergo.net@mail.ru>
2021-01-27 18:51:34 +0300

    Finish commit.

--- src/interfaces/BeforeSuite.java
+++ src/interfaces/BeforeSuite.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Test {
-    int priority();
+public @interface BeforeSuite {
+
 }
